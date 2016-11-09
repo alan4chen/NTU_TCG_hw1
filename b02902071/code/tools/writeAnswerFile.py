51282282f2e1b59e9b2e0587ffe00a5ff3c78932
alan <alan4chen@kimo.com>
2016-11-09 19:41:34 +0800

    no message

--- b02902071/code/tools/writeAnswerFile.py
+++ b02902071/code/tools/writeAnswerFile.py
@@ -2,7 +2,7 @@ import settings
 PRINT_TIME = settings.PRINT_TIME
 
 
-def writeAnswerFile(path, answers):
+def writeAnswerFile(path, answers, total_used_time=None):
     f = open(path, "w")
     for answer in answers:
         f.write("$"+str(answer[0])+"\n")
@@ -10,4 +10,6 @@ def writeAnswerFile(path, answers):
             f.write('\t'.join(map(str, line))+"\n")
         if PRINT_TIME == True:
             f.write(str(answer[2]) + "\n\n")
+    if PRINT_TIME and total_used_time != None:
+        f.write("Total Used Time: " +str(total_used_time) + "\n\n")
     f.close()
